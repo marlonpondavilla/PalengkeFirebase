e3a9c45d9ba2a3ca050125538b7d371958652185
MarlonDev <marlonpondavilla@dev.com>
2025-05-02 19:24:11 +0800

    implemented logout function

--- app/src/main/java/com/example/palengke/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/palengke/ui/settings/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.example.palengke.ui.settings;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.palengke.Login;
 import com.example.palengke.R;
+import com.example.palengke.databinding.FragmentSettingsBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsFragment extends Fragment {
 
     private SettingsViewModel mViewModel;
+    private FragmentSettingsBinding binding;
+    private FirebaseAuth auth;
+    private GoogleSignInClient googleSignInClient;
 
     public static SettingsFragment newInstance() {
         return new SettingsFragment();
@@ -25,14 +37,39 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        auth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
+
+        binding.logoutBtn.setOnClickListener(v -> {
+            auth.signOut();
+
+            googleSignInClient.signOut().addOnCompleteListener(requireActivity(), task -> {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(requireActivity(), "Logout failed", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent logout = new Intent(requireActivity(), Login.class);
+                    startActivity(logout);
+                    requireActivity().finish();
+                    Toast.makeText(requireActivity(), "Logout successfully", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+
+        return root;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
-        // TODO: Use the ViewModel
     }
-
 }
